52b47b6678784ad125832a6c7e45a7b85ab0ab9f
zhuck <zhuckovkj@gmail.com>
2022-05-29 23:37:06 +0300

    урa cum it

--- app/src/main/java/com/example/lavaro/DBHelper.java
+++ app/src/main/java/com/example/lavaro/DBHelper.java
@@ -4,39 +4,38 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
+public class DBHelper  extends SQLiteOpenHelper{
 
+    public static final int DATABASE_VERSION = 10;
+    public static final String DATABASE_NAME = "contactDb";
+    public static final String TABLE_CONTACTS = "contacts";
 
-// я тут ничего не понимал , сейчас тоже , остальное сами сделаете)
+    public static final String KEY_ID = "_id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_SURNAME = "surname";
+    public static final String KEY_PHONE = "phone";
+    public static final String KEY_ABOUT = "about";
+    public static final String KEY_CITY = "city";
+    public static final String KEY_WORKEXP = "workexp";
+    public static final String KEY_MAIL = "mail";
+    public static final String KEY_EDUCATION_PLACE = "education_place";
+    public static final String KEY_EDUCATION = "education";
 
-
-public class DBHelper extends SQLiteOpenHelper {
-
-    public static final int DATABASE_VERSION = 3;
-    public static final String DATABASE_NAME = "workers" ;
-    public static final String TABLE_WORKERS = "workers" ;
-
-    public static final String KEY_ID = "_id" ;
-    public static final String KEY_NAME = "name" ;
-    public static final String KEY_SURNAME = "surname" ;
-    public static final String KEY_PHONE = "phone" ;
-    public static final String KEY_MAIL = "mail" ;
-    public static final String KEY_PASSWORD = "password" ;
 
 
     public DBHelper(Context context) {
-        super(context,DATABASE_NAME, null, DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_WORKERS + "(" + KEY_ID
-                + " integer primary key," + KEY_NAME + " text," + KEY_SURNAME + " text," + KEY_MAIL + " text," + KEY_PHONE + " text," + KEY_PASSWORD +" text" + ")");
+        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID + " integer primary key," + KEY_CITY + " text," + KEY_NAME + " text," + KEY_SURNAME + " text," + KEY_MAIL + " text," + KEY_PHONE + " text,"  + KEY_EDUCATION_PLACE  + " text,"  + KEY_EDUCATION  + " text,"  + KEY_WORKEXP + " text," + KEY_ABOUT + " text" + ")");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_WORKERS);
+        db.execSQL("drop table if exists " + TABLE_CONTACTS);
 
         onCreate(db);
 
--- app/src/main/java/com/example/lavaro/MainActivity.java
+++ app/src/main/java/com/example/lavaro/MainActivity.java
@@ -8,74 +8,140 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button buttonRead , buttonAdd , buttonClean ;
-    EditText etMail , etPhone , etName , etSurname , etPassword;
+    Button btnAdd, btnRead, btnClear , btnAddWE ;
+    EditText etName, etSurname ,etEmail , etPlace , etYear , etAboutYou , etCity , etPhone , etEducationPlace;
+    String allWE = "";
     DBHelper dbHelper;
+    String phone, email ;
+    Spinner education ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+
+        btnAddWE = (Button) findViewById(R.id.btnAddWE);
+        btnAddWE.setOnClickListener(this);
+
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etSurname = (EditText) findViewById(R.id.etSurname);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPlace = (EditText) findViewById(R.id.etPlace);
+        etYear = (EditText) findViewById(R.id.etYear);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        etCity = (EditText) findViewById(R.id.etCity);
+        etAboutYou = (EditText) findViewById(R.id.etAboutYou);
+        etEducationPlace = (EditText) findViewById(R.id.educationPlace);
+        education =  (Spinner) findViewById(R.id.spEducation) ;
+        ArrayAdapter<?> adapter =
+                ArrayAdapter.createFromResource(this, R.array.education,
+                        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        education.setAdapter(adapter);
+
+
         dbHelper = new DBHelper(this);
-
-        buttonAdd = findViewById(R.id.btnAdd);
-        buttonAdd.setOnClickListener(this);
-        buttonClean = findViewById(R.id.btnClean);
-        buttonClean.setOnClickListener(this);
-        buttonRead = findViewById(R.id.btnClean);
-        buttonRead.setOnClickListener(this);
-
-        etMail = findViewById(R.id.email);
-        etName = findViewById(R.id.name);
-        etPhone = findViewById(R.id.phone);
-        etSurname= findViewById(R.id.surname);
-        etPassword= findViewById(R.id.password);
-
     }
-
 
     @Override
     public void onClick(View v) {
 
-        String name = etName.getText().toString() ;
-        String surname  = etSurname.getText().toString() ;
-        String phone = etPhone.getText().toString() ;
-        String email = etMail.getText().toString() ;
-        String password = etPassword.getText().toString() ;
+        String name = etName.getText().toString();
+        String surname = etSurname.getText().toString();
+        String city = etCity.getText().toString();
+        String aboutUser = etAboutYou.getText().toString();
+        String educationPlace = etEducationPlace.getText().toString();
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
 
-        SQLiteDatabase database = dbHelper.getWritableDatabase() ;
 
         ContentValues contentValues = new ContentValues();
 
 
-        switch (v.getId()) {
+        switch (v.getId()) { // С помощью этого обалденного условия смотрим на какую кнопку был нажатие
+
+            case R.id.btnAddWE:
+                String place = etPlace.getText().toString();
+                String years = etYear.getText().toString();
+                String spec =  place + "|" + years + "|" ; // формируем строку
+                allWE += spec ; // добавляем к той что для БД
+
+                break;
 
             case R.id.btnAdd:
-                contentValues.put(DBHelper.KEY_NAME, name);
-                contentValues.put(DBHelper.KEY_MAIL, email);
 
-                database.insert(DBHelper.TABLE_WORKERS, null, contentValues);
+                if (etEmail.getText().toString().matches(".+@.+\\..+")) {  // Проверка строки на то является ли строка email адресом , я очень плохую регулярку сделал но она работает
+                    email = etEmail.getText().toString();
+                }else{
+                    Toast toast = Toast.makeText(this , "Почта введена неверно" , Toast.LENGTH_LONG ); // Тут следует продумать что делать если email передан неверно
+                    email = "Почта не указана.";
+                    toast.show();
+                }
+                if (etPhone.getText().toString().matches("^(\\+7|8)[0-9]{10,11}")) { //  Тоже регулярка не очень нужно будет по хорошему другую сделать ( скорее всего я сделаю ) , она тоже рабочая но не все ситуации для записи номера обрабатывает например 8 900 015 23 46 или  8 (904) 477 43 43  или  8-904-319-32-34        .
+                    phone = etPhone.getText().toString();
+
+                }else{ // Если номер не прошел по регулярке , значение phone ( email тоже ) попадут в БД с тестом "Телефон не указан." поэтому else нужно дописать тосты бы оставил с телефона выглядит прикольно
+                    Toast toast = Toast.makeText(this , "Номер введен неверно" , Toast.LENGTH_LONG );
+                    phone = "Телефон не указан.";
+                    toast.show();
+                }
+                contentValues.put(DBHelper.KEY_MAIL, email);
+                contentValues.put(DBHelper.KEY_NAME, name);
+                contentValues.put(DBHelper.KEY_SURNAME, surname);
+                contentValues.put(DBHelper.KEY_PHONE, phone);
+                contentValues.put(DBHelper.KEY_CITY, city);
+                contentValues.put(DBHelper.KEY_ABOUT, aboutUser);
+                contentValues.put(DBHelper.KEY_EDUCATION, education.getSelectedItem().toString());
+                contentValues.put(DBHelper.KEY_EDUCATION_PLACE, educationPlace);
+                contentValues.put(DBHelper.KEY_WORKEXP, allWE);
+
+                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
                 break;
 
             case R.id.btnRead:
-                Cursor cursor = database.query(DBHelper.TABLE_WORKERS, null, null, null, null, null, null);
+                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
 
                 if (cursor.moveToFirst()) {
                     int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
                     int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+                    int surnameIndex = cursor.getColumnIndex(DBHelper.KEY_SURNAME);
+                    int workExpIndex = cursor.getColumnIndex(DBHelper.KEY_WORKEXP);
                     int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
                     int phoneIndex = cursor.getColumnIndex(DBHelper.KEY_PHONE);
-                    int passwordIndex = cursor.getColumnIndex(DBHelper.KEY_PASSWORD) ;
+                    int cityIndex = cursor.getColumnIndex(DBHelper.KEY_CITY);
+                    int aboutYouIndex = cursor.getColumnIndex(DBHelper.KEY_ABOUT);
+                    int educationIndex = cursor.getColumnIndex(DBHelper.KEY_EDUCATION);
+                    int educationPlaceIndex = cursor.getColumnIndex(DBHelper.KEY_EDUCATION_PLACE);
+
                     do {
                         Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
+                                ", city = " + cursor.getString(cityIndex) +
                                 ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex) +
-                                ", phone = " + cursor.getString(phoneIndex) +
-                                ", password = " + cursor.getString(passwordIndex));
+                                ", surname = " + cursor.getString(surnameIndex) +
+                                ", education = " + cursor.getString(educationIndex) +
+                                ", education place = " + cursor.getString(educationPlaceIndex) +
+                                ", work experience = " + cursor.getString(workExpIndex) +
+                                ", about me  = " + cursor.getString(aboutYouIndex) +
+                                ", phone  = " + cursor.getString(phoneIndex) +
+                                ", email = " + cursor.getString(emailIndex));
                     } while (cursor.moveToNext());
                 } else
                     Log.d("mLog","0 rows");
@@ -83,10 +149,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 cursor.close();
                 break;
 
-            case R.id.btnClean:;
-                database.delete(DBHelper.TABLE_WORKERS, null, null);
+            case R.id.btnClear:
+                database.delete(DBHelper.TABLE_CONTACTS, null, null);
                 break;
+
         }
         dbHelper.close();
     }
+
+
 }
